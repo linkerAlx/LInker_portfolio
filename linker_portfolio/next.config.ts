2f6858ce83b2f812994f819fba5dde2bfa2d644f
linkerAlx <linker222006766@gmail.com>
2025-01-03 22:43:46 +0200

    LInker new portfolio

---
+++ linker_portfolio/next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  
+    images: {
+      domains: ['images.pexels.com'], // Add your image domain here
+    },
+  
+  
+};
+
+export default nextConfig;
+
